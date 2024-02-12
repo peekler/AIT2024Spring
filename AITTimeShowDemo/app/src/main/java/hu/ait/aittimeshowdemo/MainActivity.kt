575660e19a3d2daf453b37f5049a74a82d0f1407
Péter Ekler <peter.ekler@aut.bme.hu>
2024-02-12 22:45:14 +0100

    Class 03 demo
    Time Show demo:
    - String extraction
    - SnackBar
    - EditText

--- AITTimeShowDemo/app/src/main/java/hu/ait/aittimeshowdemo/MainActivity.kt
+++ AITTimeShowDemo/app/src/main/java/hu/ait/aittimeshowdemo/MainActivity.kt
@@ -1,13 +1,20 @@
 package hu.ait.aittimeshowdemo
 
+
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewAnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.google.android.material.snackbar.Snackbar
 import hu.ait.aittimeshowdemo.databinding.ActivityMainBinding
 import java.util.Date
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,13 +30,51 @@ class MainActivity : AppCompatActivity() {
         //val btnShow = findViewById<Button>(R.id.btnShow)
 
         binding.btnShow.setOnClickListener {
+
+
+            //if (binding.etName.text.isNotEmpty()) { }
+
             val current = Date(System.currentTimeMillis()).toString()
+            val userName = binding.etName.text.toString()
 
-            binding.tvData.text=current
+            binding.tvData.text="$userName time: $current"
 
-            Toast.makeText(this, current, Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, "hello $userName", Toast.LENGTH_LONG).show()
+
+            Snackbar.make(
+                binding.root, "hello $userName", Snackbar.LENGTH_LONG
+            ).setAction("Undo"
+            ) {
+                // code..
+            }.show()
+
+            Log.d("TAG_DEMO", "Button pressed $current")
+
+            revealCard()
         }
+    }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun revealCard() {
+        val x = binding.cardView.getRight()
+        val y = binding.cardView.getBottom()
+
+        val startRadius = 0
+        val endRadius = Math.hypot(binding.cardView.getWidth().toDouble(),
+            binding.cardView.getHeight().toDouble()).toInt()
+
+        val anim = ViewAnimationUtils.createCircularReveal(
+            binding.cardView,
+            x,
+            y,
+            startRadius.toFloat(),
+            endRadius.toFloat()
+        )
+
+        //anim.duration = 5000
+
+        binding.cardView.setVisibility(View.VISIBLE)
+        anim.start()
     }
 
 }
